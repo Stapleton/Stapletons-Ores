e0d020bf83f9dcdfafdb0ba6f9194bc8b11507c7
Taylor Stapleton <stapletontaylor@hotmail.com>
2018-02-12 13:16:45 -0700

    Remove Soft Dep on Content Tweaker

--- src/main/java/stapleton/stapletonsores/StapletonsOres.java
+++ src/main/java/stapleton/stapletonsores/StapletonsOres.java
@@ -11,7 +11,7 @@ import org.apache.logging.log4j.Logger;
 import stapleton.stapletonsores.block.Ores;
 import stapleton.stapletonsores.proxy.CommonProxy;
 
-@Mod(useMetadata = true, modid = StapletonsOres.MOD_ID, dependencies = "before:contenttweaker")
+@Mod(useMetadata = true, modid = StapletonsOres.MOD_ID)
 public class StapletonsOres {
 
     public static final String MOD_ID = "stapletonsores";
